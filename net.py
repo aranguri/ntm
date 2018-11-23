f93ee9bc5d4982054d565bc1ed938547b322d97b
Santiago Aranguri <santiago.aranguri@gmail.com>
2018-11-22 21:32:17 -0800

    Continued trying to improve performance over LSTM

--- net.py
+++ net.py
@@ -9,17 +9,19 @@ beta2 = 0.999
 memory_enabled = True
 
 batch_size = 32
-memory_size = 7
-input_length = 7
-output_length = 7
-input_size = 1
+
+memory_length = 128
+memory_size = 20
+input_length = 8
+output_length = 8
+input_size = 8
 output_size = input_size
 h_size_i = 4
 h_size_m = 4
 
-x = tf.placeholder(tf.float32, shape=(batch_size, input_length + output_length))
-y = tf.placeholder(tf.float32, shape=(batch_size, input_length + output_length))
-initial_memories = tf.Variable(tf.constant(1e-6, shape=(1, batch_size, memory_size)))
+x = tf.placeholder(tf.float32, shape=(batch_size, input_length + output_length, input_size))
+y = tf.placeholder(tf.float32, shape=(batch_size, input_length + output_length, output_size))
+initial_memories = tf.Variable(tf.constant(1e-6, shape=(1, batch_size, memory_length, memory_size)))
 initial_memories = tf.stop_gradient(initial_memories) #Avoid backprop on initial memory
 
 rnn_i = LSTM(tf.zeros([2, batch_size, h_size_i]), memory_size)
@@ -33,7 +35,7 @@ def body(output, memories, *rnn_hs):
     rnn_i.h, rnn_m.h = rnn_hs
     # the memories variable stores the history of all previous states of the memory
     x_now = x[:, tf.shape(output)[1]]
-    x_now = tf.reshape(x_now, shape=(batch_size, 1))
+    x_now = tf.reshape(x_now, shape=(batch_size, 1, input_size))
 
     if memory_enabled:
         memory = memories[-1]
@@ -41,7 +43,6 @@ def body(output, memories, *rnn_hs):
         input_and_all_memory = tf.concat((x_now, memory), axis=1)
         memory_attn = tf.nn.softmax(rnn_i(input_and_all_memory))
         selected_memory = tf.reduce_sum(memory_attn * memory, axis=1, keepdims=True)
-
         input_memory = tf.concat((x_now, selected_memory), axis=1)
         new_memory, memory_gate, extra_output = tf.split(rnn_m(input_memory), [memory_size, 1, output_size], axis=1)
 
@@ -61,9 +62,9 @@ def body(output, memories, *rnn_hs):
     return new_output, memories, rnn_i.h, rnn_m.h
 
 #is there a better way of doing this?
-shapes = [tf.TensorShape([batch_size, None]), tf.TensorShape([None, batch_size, memory_size]), tf.TensorShape([2, batch_size, h_size_i]),
+shapes = [tf.TensorShape([batch_size, None, output_size]), tf.TensorShape([None, batch_size, memory_length, memory_size]), tf.TensorShape([2, batch_size, h_size_i]),
           tf.TensorShape([2, batch_size, h_size_m])]
-last_state = tf.while_loop(cond, body, [tf.constant(0.0, shape=(batch_size, 0)), initial_memories, rnn_i.h, rnn_m.h], shape_invariants=[*shapes])
+last_state = tf.while_loop(cond, body, [tf.constant(0.0, shape=(batch_size, 0, output_size)), initial_memories, rnn_i.h, rnn_m.h], shape_invariants=[*shapes])
 output, memories = last_state[0:2]
 
 optimizer = tf.train.AdamOptimizer(learning_rate, beta1, beta2)
@@ -78,7 +79,8 @@ with tf.Session() as sess:
     train_writer = tf.summary.FileWriter('./logs/9/train ', sess.graph)
 
     for i in itertools.count():
-        bits = np.random.randn(batch_size, input_length)
+        bits = np.random.randint(0, 2, shape=(batch_size, input_length))
+        print(bits)
         xs = np.concatenate((bits, np.zeros_like(bits)), axis=1)
         ys = np.concatenate((np.zeros_like(bits), bits), axis=1)
         tr_loss[i], _, output_, memories_ = sess.run([loss, minimize, output, memories], feed_dict={x: xs, y: ys})
